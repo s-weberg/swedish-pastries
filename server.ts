9fd31016ec0c49e0d09dde0c042e841acb56ac5c
s-weberg <sandraweberg90@gmail.com>
2025-09-29 15:47:45 +0200

    Updated get, post, push
    Errors, needs work

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { z } from "zod";
+import { de } from "zod/v4/locales";
 
 const app = express();
 
@@ -7,21 +8,54 @@ app.use(express.json());
 
 const PORT = 3000;
 
-//This is the pastry schema with zod
+let pastries = [
+    {
+        name: "Hallongrotta",
+        description: "A classic pastry filled with raspberry jam.",
+        price: 15,
+    },
+    {
+        name: "Kärleksmums",
+        description: "Chocolate cake with coconut topping.",
+        price: 25,
+    },
+    {
+        name: "Kanelbulle",
+        description: "Sweet bun with cinnamon and sugar.",
+        price: 20,
+    }
+];
+
+//This is the pastry schema with zod. z.object defines a schema for an object.
 const SwedishPastrySchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    price: z.number()
+    name: z.string().min(1).max(100),
+    description: z.string().min(1).max(300),
+    price: z.number().min(10).max(100),
+})
+
+
+app.get('/pastries', (req, res) => {
+  res.json(SwedishPastrySchema);
+});
+
+app.post('/pastries', (req, res) => {
+    const newPastry = req.body;
+    name: pastries.length + 1;
+    description: req.body.description;
+    price: req.body.price;
+
+    pastries.push(newPastry);
+    res.json({ message: "New pastry added", pastry: newPastry });
 })
 
 
 
 
-// id: 1, name: "", price?:""
 
-app.get('/', (req, res) => {
-  res.send('Hello, Swedish Pastries!');
-});
+
+
+
+app.post('/')
 
 
 
